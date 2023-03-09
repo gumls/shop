95ea2537b129d40f281e718de4e48776062f8d0e
gml <1174252658@qq.com>
2023-03-09 17:26:52 +0800

    微信退款

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,5 +15,6 @@ class VerifyCsrfToken extends Middleware
         //
         "payment/alipay/notify", //支付宝回调地址 不会有 CSRF Token
         "payment/wechat/notify", //微信支付回调
+        'payment/wechat/refund_notify', //微信退款回调
     ];
 }
