891d7c541446c2427c1aa019d35a32c5fe8d3425
gml <1174252658@qq.com>
2023-03-13 11:46:12 +0800

    优惠券新增

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -61,7 +61,13 @@ class CouponCodesController extends AdminController
 
         $form->display('id', 'ID');
         $form->text('name', '名称')->rules('required');
-        $form->text('code', '优惠码')->rules('nullable|unique:coupon_codes');
+        $form->text('code', '优惠码')->rules(function ($v){
+            if($id = $v->model()->id){
+                return 'nullable|unique:coupon_codes,code,'.$id.'id';
+            } else {
+                return 'nullable|unique:coupon_codes';
+            }
+        });
         $form->radio('type', '类型')->options(CouponCode::$typeMap)->rules('required')->default(CouponCode::TYPE_FIXED);
         $form->text('value', '折扣')->rules(function ($form) {
             if (request()->input('type') === CouponCode::TYPE_PERCENT) {
