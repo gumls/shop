8d10d6847fe0f4e82c11277ef7091ab60ab4ac58
gml <1174252658@qq.com>
2023-03-21 16:44:48 +0800

    类目

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // 放在 ProductsSeeder 之前
+        $this->call(CategoriesSeeder::class);
+        $this->call(ProductsSeeder::class);
     }
 }
